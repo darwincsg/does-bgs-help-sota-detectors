8d8c7df0b911d4eb922bf7396cb7c87079c38885
Joel Leiva <63426856+darwincsg@users.noreply.github.com>
2025-05-13 20:18:08 +0100

    Update main.py
    Simplifying methods

--- main.py
+++ main.py
@@ -1,39 +1,55 @@
-from scripts.cleans import clean_directory, clean_txt_of_xml, clean_txt_of_yolo
-from scripts.file_functions import convert_cvat_xml_to_yolo
-from scripts.IoU_function import get_Average
+#import argparse
+from ultralytics import YOLO
 from pathlib import Path
-import argparse
+import os
 
-def main(arg_XML, arg_YOLO, arg_Frame):
-    path_XML = arg_XML.strip().split('/')
+from scripts.file_functions import convert_cvat_xml_to_abs
+from scripts.models import Yolo_predict
+from scripts.IoU_function import get_Average
 
-    archivo_Xml = Path(path_XML[len(path_XML)-1])
-  
-    if archivo_Xml.suffix == ".xml":
-        absolute_name = archivo_Xml.stem
-        directory_name = f'/home/{absolute_name}'
-    else:
-        raise ValueError("The file is not a valid XML.")
 
-    convert_cvat_xml_to_yolo(arg_XML,directory_name)
 
-    clean_directory(directory_name,'XML')
+def build(video_path):
+    video_split = video_path.strip().split('/')
 
-    clean_directory(arg_YOLO,'YOLO')
+    video_file = Path(video_split[len(video_split)-1])
+    absolute_name = video_file.stem
+    
+    model_name = "yolov8x.pt"
+    model = YOLO(model_name)
 
-    final = get_Average(directory_name,arg_YOLO,arg_Frame)
+    #CREATE XML DIRECTORY AND FILE NAME
+    Xml_Path = f'/home/darwonl/Escritorio/PROJECT/ACC/Modelos/YOLO/Yolov8/xml/{absolute_name}'
+    os.makedirs(Xml_Path, exist_ok=True)
+    Xml_file = f'/home/darwonl/Escritorio/PROJECT/ACC/Modelos/Detection/Outdoor/Outdoor/{absolute_name}.xml'
+    
+    #CREATE YOLO DIRCTORY
+    Yolo_Path = f'/home/darwonl/Escritorio/PROJECT/ACC/Modelos/YOLO/Yolov8/yolo/{absolute_name}'
+    os.makedirs(Yolo_Path, exist_ok=True)
+    
+    #First part, we sent the xml and convert it to txt files on the output dir
+    convert_cvat_xml_to_abs(Xml_file, Xml_Path)
+    
+    #Second part, call Yolo and made the predictions
+    Yolo_predict(model,video_path, Yolo_Path)
+    
+    #Final part, do the comparison!
+    final = get_Average(Xml_Path,Yolo_Path)
+    
+    return final
+
+
+def main():
+
+    path_video = '/home/darwonl/Escritorio/PROJECT/ACC/Modelos/Detection/Raw/Raw/099_012_07_05_2024_10_15_SRM_5.8_10_30.MP4'
+
+    final = build(path_video)
 
     print(final)
 
 
 
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Script to obtain the total accuracy of a Yolo annotation")
-
-    parser.add_argument("arg1", type=str, help="Grand True path")
-    parser.add_argument("arg2", type=str, help="Yolo annotation path")
-    parser.add_argument("arg3", type=str, help="Frame path")
-
-    args = parser.parse_args()
-    
-    main(args.arg1, args.arg2, args.arg3)
+    main()
